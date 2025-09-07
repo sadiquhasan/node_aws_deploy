c8817353ceaff4d644f42af096d71602ec0e0945
Mohammad sadiqu <60215319+sadiquhasan@users.noreply.github.com>
2025-09-07 19:15:44 +0530

    Delete hrm directory

--- hrm/helper/auth_helper.js
+++
@@ -1,9 +0,0 @@
-const ensureAuthenticated = function (req, res, next) {
-  if (req.isAuthenticated()) {
-    next();
-  }
-  console.log("Error Message", "You are not authorized user");
-  res.redirect("/auth/login", 302, {});
-};
-
-module.exports = ensureAuthenticated;
