7e53f6a09887fc2615e7ec756ee4dffc10a4786d
mohammad sadiqu <mdsadiqu@quaratech.com>
2025-09-07 20:07:33 +0530

    correction

--- express practice/server.js
+++ express practice/server.js
@@ -1,9 +1,9 @@
-require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const Product = require("./models/products");
-const bodyparser = require("body-parser");
 const app = express();
+
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
